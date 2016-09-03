8457967fbb2e74e2ee7986690633a02616c6a9e6
Matt Campbell <matt@mattcampbell.net>
2016-09-03 11:24:27 -0400

    Track assets outside of web/ output folder

--- tasks/serve.js
+++ tasks/serve.js
@@ -4,9 +4,20 @@ var serveStatic = require('serve-static');
 var http = require('http');
 var config = require('./config');
 
+var filterServeStatic = function(regexp, middleware) {
+  return function(req, res, next) {
+    if (regexp.test(req.url)) {
+      return middleware(req, res, next);
+    }
+
+    next();
+  };
+};
+
 module.exports = function() {
   var server = connect();
   server.use(compression());
+  server.use(filterServeStatic(/^\/assets\//i, serveStatic('./')));
   server.use(serveStatic(config.web));
 
   http.createServer(server).listen(config.port);
